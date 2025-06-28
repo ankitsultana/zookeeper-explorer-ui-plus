25f2acd540efbc65f661c5fa777f0b5901379334
Ankit Sultana <ankitsultana@gmail.com>
2025-06-28 13:50:28 -0500

    force commit

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  base: '/zookeeper-explorer-ui-plus/',
   plugins: [
     react(),
     mode === 'development' &&
